0eb98043316c18cda116febbcf570ef79c8f0f1e
Thinh Tran <quangthinhtran3588@gmail.com>
2021-05-11 19:09:38 +0700

    add dump test script

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,7 @@
 import {log} from '@core';
 import {execute as initializeApp} from './initialize-app';
 import {execute as migrateDatabase} from './migrate-database';
+import {execute as test} from './test';
 
 const execute = () => {
   const args = process.argv;
@@ -12,6 +13,7 @@ const execute = () => {
   const scripts = {
     'initialize-app': initializeApp,
     'migrate-database': migrateDatabase,
+    test,
   };
   if (!scripts[args[2]]) {
     log.warn('Invalid script name');
