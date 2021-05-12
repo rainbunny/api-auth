6fb8a568b97cccd9b9542cc27f3dbf3a6f2e268f
Thinh Tran <quangthinhtran3588@gmail.com>
2021-05-12 15:01:25 +0700

    refactor server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import helmet from 'helmet';
 import {ApolloServer} from 'apollo-server-express';
+import {log} from '@core';
 import {configureServer} from './server.config';
 
 const {apolloServerConfig} = configureServer();
 const port = process.env.PORT || 3000;
-const server = express();
-server.use(helmet());
+const server = express().use(helmet());
 const apolloServer = new ApolloServer(apolloServerConfig);
 apolloServer.applyMiddleware({app: server});
 server.listen({port}, () =>
   // eslint-disable-next-line no-console
-  console.log(`🚀 Server ready at http://localhost:${port}${apolloServer.graphqlPath}`),
+  log.info(`🚀 Server ready at http://localhost:${port}${apolloServer.graphqlPath}`),
 );
