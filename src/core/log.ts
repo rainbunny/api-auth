7230fdb10854f1683d7cfcaf7b69c388b2930462
Thinh Tran <quangthinhtran3588@gmail.com>
2021-05-12 15:06:13 +0700

    fix linting error

--- src/core/log.ts
+++ src/core/log.ts
@@ -1,6 +1,9 @@
 import bunyan from 'bunyan';
 import {config} from '@rainbunny/api-core';
 
-const log = bunyan.createLogger({name: config().APP_NAME, level: process.env.LOG_LEVEL as bunyan.LogLevel});
+const log = bunyan.createLogger({
+  name: config().APP_NAME,
+  level: (process.env.LOG_LEVEL as unknown) as bunyan.LogLevel,
+});
 
 export {log};
