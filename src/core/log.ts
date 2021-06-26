f6a78d29172febd7a3df25ecd178c4da85e2982d
Thinh Tran <quangthinhtran3588@gmail.com>
2021-06-26 15:29:29 +0700

    update log

--- src/core/log.ts
+++ src/core/log.ts
@@ -1,9 +1,27 @@
-import bunyan from 'bunyan';
+/* eslint-disable no-console */
 import {config} from '@tqt/api-core';
+import bunyan from 'bunyan';
 
-const log = bunyan.createLogger({
-  name: config().APP_NAME,
-  level: process.env.LOG_LEVEL as unknown as bunyan.LogLevel,
-});
+const bunyanLog = bunyan.createLogger({name: config().APP_NAME, level: process.env.LOG_LEVEL as bunyan.LogLevel});
 
+const debug = {
+  info: (...params) => {
+    params.forEach((val) => console.log(val));
+    return true;
+  },
+  error: (...params) => {
+    params.forEach((val) => console.error(val));
+    return true;
+  },
+  debug: (...params) => {
+    params.forEach((val) => console.debug(val));
+    return true;
+  },
+  warn: (...params) => {
+    params.forEach((val) => console.warn(val));
+    return true;
+  },
+} as unknown as typeof bunyanLog;
+
+const log = process.env.NODE_ENV === 'develop' ? debug : bunyanLog;
 export {log};
