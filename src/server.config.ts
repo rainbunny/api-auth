49d32b8acb4191c4a9e1a344d81850ea0faa4312
Thinh Tran <quangthinhtran3588@gmail.com>
2021-06-26 18:27:51 +0700

    fix Pool configuration

--- src/server.config.ts
+++ src/server.config.ts
@@ -3,7 +3,7 @@ import {defaults} from 'pg';
 import {asValue, AwilixContainer, createContainer, InjectionMode} from 'awilix';
 import _ from 'lodash/fp';
 import * as firebaseAdmin from 'firebase-admin';
-import {RxPool} from '@tqt/pg-extensions';
+import {Pool} from '@tqt/pg-extensions';
 import {baseTypeDefs, baseResolvers, log, updateConfig, contextFactory} from '@core';
 import {typeDefs as authTypeDefs, resolvers as authResolvers} from '@auth/graphql';
 import {bootstrap as bootstrapAuth} from '@auth/bootstrap';
@@ -19,7 +19,7 @@ const logPool = (
       },
 ): void => {
   if (typeof message !== 'string') {
-    log.info(message);
+    log.debug(message);
   }
 };
 
@@ -33,25 +33,25 @@ export const configureServer = (): {apolloServerConfig: any; container: AwilixCo
 
   // setup database
   defaults.parseInt8 = true;
-  const writePool = new RxPool({
+  const writePool = new Pool({
     host: process.env.POSTGRES_HOST,
     port: +process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: Boolean(process.env.POSTGRES_SSL),
+    ssl: 'true'.localeCompare(process.env.POSTGRES_SSL, undefined, {sensitivity: 'base'}) === 0,
     log: logPool,
   })
     .on('error', (err) => log.error(err))
     .on('connect', () => log.info('Connected to write database'));
 
-  const readPool = new RxPool({
+  const readPool = new Pool({
     host: process.env.REPLICA_POSTGRES_HOST,
     port: +process.env.REPLICA_POSTGRES_PORT,
-    database: process.env.REPLICA_POSTGRES_DB,
+    database: process.env.REPLICA_POSTGRES_DATABASE,
     user: process.env.REPLICA_POSTGRES_USER,
     password: process.env.REPLICA_POSTGRES_PASSWORD,
-    ssl: Boolean(process.env.REPLICA_POSTGRES_SSL),
+    ssl: 'true'.localeCompare(process.env.REPLICA_POSTGRES_SSL, undefined, {sensitivity: 'base'}) === 0,
     log: logPool,
   })
     .on('error', (err) => log.error(err))
@@ -61,8 +61,8 @@ export const configureServer = (): {apolloServerConfig: any; container: AwilixCo
   const container = createContainer({
     injectionMode: InjectionMode.PROXY,
   }).register({
-    readDatabase: asValue<RxPool>(readPool),
-    writeDatabase: asValue<RxPool>(writePool),
+    readDatabase: asValue<Pool>(readPool),
+    writeDatabase: asValue<Pool>(writePool),
   });
   bootstrapAuth(container);
 
